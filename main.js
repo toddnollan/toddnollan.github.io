ff1d41529673f4c3ee35ce5e46b38b1bbf4a1087
toddnollan <toddnollan@gmail.com>
2019-12-15 04:49:58 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-const scriptVersion = "Script v.004"; //declare version, write to main
+const scriptVersion = "Script v.005"; //declare version, write to main
 document.getElementById("versionlabel").innerHTML = scriptVersion;
 //Find and assign the three output panes
 const hullsOut = document.getElementById("hullspane");
 const settingsOut = document.getElementById("settingspane");
 const viewportOut = document.getElementById("viewport");
+
+hullsOut.innerHTML = "Sometimes, I dream about cheese."

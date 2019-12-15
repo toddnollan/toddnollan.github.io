d061918fd2d8ff7f0510a2be9caa0b8b74b17ec9
toddnollan <toddnollan@gmail.com>
2019-12-14 23:00:00 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const scriptVersion = "Script v.002";
+const scriptVersion = "Script v.003<br>"; //declare version, write to main
 document.getElementById("versionlabel").innerHTML = scriptVersion;
+//Find and assign the three output panes
 const hullsOut = document.getElementById("hullspane");
 const settingsOut = document.getElementById("settingspane");
 const viewportOut = document.getElementById("viewport");
